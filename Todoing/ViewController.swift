42b51c58922a528da693928c4926f970786eee3a
Jinal <jinal@artixun.com>
2020-07-13 19:15:29 +0530

    Initial Commit

---
+++ Todoing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoing
+//
+//  Created by Artixun on 7/13/20.
+//  Copyright © 2020 Pk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
