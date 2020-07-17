b54eeeaebaae0972972dc6a0e68c9b1867a7b2fc
Jinal <jinal@artixun.com>
2020-07-17 18:23:55 +0530

    CRUD functionality add and start adding of search bar.

--- Todoing/Controllers/TodoListViewController.swift
+++ Todoing/Controllers/TodoListViewController.swift
@@ -7,21 +7,22 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     
     var itemArray = [Item]()   // array of Item objects
     // let defaults = UserDefaults.standard // this is a singleton for user default setting
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist") // this use when we save data in plist file
+    // here UIApplication.shared is a singleton
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // here UIApplication.shared is use to create object from AppDelegate class method
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-
-        
-        print(dataFilePath!)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
 //        let newItem = Item()  // new object is created with help of Item class in Data model folder
 //        newItem.title = "Find Mike"
@@ -80,6 +81,9 @@ class TodoListViewController: UITableViewController {
 //            itemArray[indexPath.row].done = false
 //        }
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done // work same as above if else code
+
+//        context.delete(itemArray[indexPath.row])  // this delete data from source
+//        itemArray.remove(at: indexPath.row)  // this load up the data source and remove item from item array
         
         saveItems()
         
@@ -106,8 +110,11 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "AddItem", style: .default) { (action) in
             // what will happen once user click on the Add Item button
             
-            let newItem = Item()
+            
+
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             
             self.itemArray.append(newItem)
             //self.defaults.set(self.itemArray, forKey: "TodoListArray") // add data in user default
@@ -128,29 +135,46 @@ class TodoListViewController: UITableViewController {
     
     // MARK:- Model Manupulation Methods
     func saveItems() {
-        let encoder = PropertyListEncoder() // this encode data type into plist
         
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         
         // to get new data into itemArray
         tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-                let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data) // here Item is data type
-            }catch {
-                print("Error decoding item array \(error)")
-            }
-            
-            }
+    func loadItems() { // to read data from Item entity
+        let request:NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fatching data from context \(error)")
         }
+    }
+    
+
 }
 
+//MARK:- search bar
+// this extension use when too much delegate are added with same class this is extension of above class
+extension TodoListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        // [cd] c is for CASE sensitive and d is for diacritic sensitive
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)  // %@ use for any argument pass in
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        //request.sortDescriptors = [sortDescriptor]
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fatching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+}
